3c4a4370de0ee9fd33ccf6dba4257436d77259a0
djohar <djohar.mouhoumed@gmail.com>
2018-11-15 15:01:31 +0100

    first commi

---
+++ TP-Djohar/src/main/java/com/intergiciel/tp/TpDjoharApplication.java
@@ -0,0 +1,12 @@
+package com.intergiciel.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDjoharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpDjoharApplication.class, args);
+	}
+}
---
+++ TP-Djohar/src/test/java/com/intergiciel/tp/TpDjoharApplicationTests.java
@@ -0,0 +1,16 @@
+package com.intergiciel.tp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TpDjoharApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
